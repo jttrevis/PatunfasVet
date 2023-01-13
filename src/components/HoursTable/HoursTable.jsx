337b81a204e7455b053e0d8d739cac04fc5a277c
jttrevis <jr.trevis@hotmail.com>
2023-01-13 21:43:52 +0000

    styles added

--- src/components/HoursTable/HoursTable.jsx
+++ src/components/HoursTable/HoursTable.jsx
@@ -2,7 +2,7 @@ export const HoursTable = () => {
 	return (
 		<>
 			<div className=''>
-				<div className='hoursBlock'>
+				<div className='hoursBlock md:w-[700px] bg-cyan-100/10 rounded-2xl'>
 					<ul>
 						<li>
 							Mon <span>7:00am - 5:30pm</span>
