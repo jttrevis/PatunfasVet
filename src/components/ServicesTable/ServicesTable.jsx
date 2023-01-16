6dfa6beff45ed6e6c65ee783eaf5971896dd0525
jttrevis <jr.trevis@hotmail.com>
2023-01-16 13:24:14 +0000

    added new photos

--- src/components/ServicesTable/ServicesTable.jsx
+++ src/components/ServicesTable/ServicesTable.jsx
@@ -4,14 +4,46 @@ export const ServicesTable = () => {
 	return (
 		<div className='md:container '>
 			<section className='flex flex-col gap-16  m-12 md:flex-row'>
-				<div className='serviceBlock  shadow-2xl bg-emergency hover:animate-pulse hover:scale-110 duration-500  bg-cover bg-center'>
-					<Link to='/'>Emergencies</Link>
+				<div className='flex text-center flex-col justify-center items-center hover:scale-110 duration-500'>
+					<Link
+						className='text-2xl font-bold'
+						to='/emergency'
+					>
+						<img
+							src='/images/dogNurser.png'
+							alt='vet dog'
+							className='serviceBlock flex-col shadow-2xl   '
+						/>
+						<span>Emergency</span>
+					</Link>
 				</div>
-				<div className='serviceBlock  shadow-2xl bg-appointment hover:animate-pulse hover:scale-110 duration-500  bg-cover bg-center '>
-					<Link to='/appointment'>Make an appointment</Link>
+
+				<div className='flex flex-col text-center justify-center items-center hover:scale-110 duration-500 '>
+					<Link
+						className='text-2xl font-bold'
+						to='/appointment'
+					>
+						<img
+							src='/images/appCat.png'
+							alt='vet dog'
+							className='serviceBlock flex-col shadow-2xl  '
+						/>
+						<span>Make an appointment</span>
+					</Link>
 				</div>
-				<div className='serviceBlock  shadow-2xl bg-other hover:animate-pulse hover:scale-110 duration-500 bg-cover bg-center'>
-					<Link to='/'>Others</Link>
+
+				<div className='flex flex-col text-center justify-center items-center hover:scale-110 duration-500'>
+					<Link
+						className='text-2xl font-bold'
+						to='/others'
+					>
+						<img
+							src='/images/doccat2.png'
+							alt='vet dog'
+							className='serviceBlock flex-col shadow-2xl   '
+						/>
+						<span>Others</span>
+					</Link>
 				</div>
 			</section>
 		</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,7 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,jsx}'],
 	theme: {
-		extend: {
-			backgroundImage: {
-				emergency: "url('../public/images/eBg.jpg')",
-				appointment: "url('../public/images/aBg.jpg')",
-				other: "url('../public/images/oBg.jpg')",
-			},
-		},
+		extend: {},
 	},
 	plugins: [],
 };
